91eb37f5dd25b02c76ed316a7da41513241e50a6
Davert <davert.ua@gmail.com>
2019-06-08 19:35:46 +0300

    fixed tests

--- scripts/ci.android.sh
+++ scripts/ci.android.sh
@@ -1,21 +1,17 @@
 #!/bin/bash -e
 
 # Approve unapproved SDK licenses
-yes | $ANDROID_HOME/tools/bin/sdkmanager --licenses
+#  yes | $ANDROID_HOME/tools/bin/sdkmanager 
 
 source $(dirname "$0")/ci.sh
 
-pushd detox/android
-run_f "./gradlew test"
-popd
 
-pushd detox/test
+pushd test
 # Workaround until react android issue will be fixed - react-native: 0.55
-mv node_modules/react-native/ReactAndroid/release.gradle node_modules/react-native/ReactAndroid/release.gradle.bak
-cp extras/release.gradle node_modules/react-native/ReactAndroid/
+# mv node_modules/react-native/ReactAndroid/release.gradle node_modules/react-native/ReactAndroid/release.gradle.bak
+# cp extras/release.gradle node_modules/react-native/ReactAndroid/
 
 run_f "npm run build:android"
 run_f "npm run e2e:android-ci"
-cp coverage/lcov.info coverage/e2e.lcov
-# run_f "npm run verify-artifacts:android"
+
 popd
--- scripts/ci.ios.sh
+++ scripts/ci.ios.sh
@@ -2,11 +2,7 @@
 
 source $(dirname "$0")/ci.sh
 
-run_f "$(dirname "$0")/unit.ios.sh"
-
-pushd detox/test
+pushd test
 run_f "npm run build:ios"
 run_f "npm run e2e:ios-ci"
-cp coverage/lcov.info coverage/e2e.lcov
-# run_f "npm run verify-artifacts:ios"
 popd
