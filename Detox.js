3424685096154fe8956f217b5f53e0f9e1de52c8
Davert <davert.ua@gmail.com>
2019-06-13 01:30:15 +0300

    fixed build, updated tests

--- Detox.js
+++ Detox.js
@@ -1,4 +1,5 @@
 const Helper = require('codeceptjs').helper;
+const recorder = require('codeceptjs').recorder;
 const path = require('path');
 
 let detox;
@@ -98,90 +99,182 @@ class Detox extends Helper {
     });
   }
 
+  /**
+   * Execute code only on iOS
+   *
+   * ```js
+   * I.runOnIOS(() => {
+    *    I.click('Button');
+    *    I.see('Hi, IOS');
+    * });
+    * ```
+    *
+    */
+   async runOnIOS(fn) {
+     if (device.getPlatform() !== 'ios') return;
+     recorder.session.start('iOS-only actions');
+     fn();
+     recorder.add('restore from iOS session', () => recorder.session.restore());
+     return recorder.promise();
+  }
+
+
+  /**
+   * Execute code only on iOS
+   *
+   * ```js
+   * I.runOnAndroid(() => {
+    *    I.click('Button');
+    *    I.see('Hi, Android');
+    * });
+    * ```
+    *
+    */
+   async runOnAndroid(fn) {
+     if (device.getPlatform() !== 'android') return;
+     recorder.session.start('Android-only actions');
+     fn();
+     recorder.add('restore from Android session', () => recorder.session.restore());
+     return recorder.promise();
+  }  
+
+
   tap(locator) {
     return this.click(locator);
   }
 
+  async multiTap(locator, num, context = null) {
+    locator = this._detectLocator(locator);
+    if (context) locator = this._detectLocator(context).withDescendant(locator);
+    await element(this._detectLocator(locator)).multiTap(num);
+  }
+
+  async longPress(locator, sec, context = null) {
+    locator = this._detectLocator(locator);
+    if (context) locator = this._detectLocator(context).withDescendant(locator);
+    await element(this._detectLocator(locator)).longPress(sec * 1000);
+  }
+
   async click(locator, context) {
-    locator = detectLocator(locator);
-    if (context) locator = detectLocator(context).withDescendant(locator);
-    await element(detectLocator(locator)).tap();
+    locator = this._detectLocator(locator);
+    if (context) locator = this._detectLocator(context).withDescendant(locator);
+    await element(this._detectLocator(locator)).tap();
+  }
+
+  async clickAtPoint(locator, x, y) {
+    await element(this._detectLocator(locator)).tapAtPoint({ x, y });
   }
 
   see(text, context = null) {
     if (context) {
-      return expect(element(detectLocator(context))).toHaveText(text);
+      return expect(element(this._detectLocator(context))).toHaveText(text);
     }
     return expect(element(by.text(text))).toExist();
   }
 
   dontSee(text, context = null) {
     let locator = by.text(text);
-    if (context) locator = detectLocator(context).withDescendant(locator);
+    if (context) locator = this._detectLocator(context).withDescendant(locator);
     return expect(element(locator)).toNotExist();
   }
 
   seeElement(locator, context = null) {
-    locator = detectLocator(locator);
-    if (context) locator = detectLocator(context).withDescendant(locator);
+    locator = this._detectLocator(locator);
+    if (context) locator = this._detectLocator(context).withDescendant(locator);
     return expect(element(locator)).toExist();
   }
 
   dontSeeElement(locator, context = null) {
-    locator = detectLocator(locator);
-    if (context) locator = detectLocator(context).withDescendant(locator);
+    locator = this._detectLocator(locator);
+    if (context) locator = this._detectLocator(context).withDescendant(locator);
     return expect(element(locator)).toNotExist();
   }
 
   async fillField(field, value) {
-    await element(field).tap();
-    await element(field).replaceText(value);
+    await element(this._detectLocator(field)).tap();
+    await element(this._detectLocator(field)).replaceText(value);
   }
 
   async clearField(field) {
-    await element(field).tap();
-    await element(field).clearText();
+    await element(this._detectLocator(field)).tap();
+    await element(this._detectLocator(field)).clearText();
   }
 
   async appendField(field, value) {
-    await element(field).tap();
-    await element(field).typeText(value);
+    await element(this._detectLocator(field)).tap();
+    await element(this._detectLocator(field)).typeText(value);
   }
+
+  async scrollTop(locator) {
+    await element(this._detectLocator(locator)).scrollTo('top');
+  }
+
+  async scrollBottom(locator) {
+    await element(this._detectLocator(locator)).scrollTo('bottom');
+  }
+
+  async scrollLeft(locator) {
+    await element(this._detectLocator(locator)).scrollTo('left');
+  }
+
+  async scrollRight(locator) {
+    await element(this._detectLocator(locator)).scrollTo('right');
+  }
+
+  async swipeTop(locator, speed = 'slow') {
+    await element(this._detectLocator(locator)).swipe('top', speed);
+  }
+
+  async swipeBottom(locator, speed = 'slow') {
+    await element(this._detectLocator(locator)).swipe('bottom', speed);
+  }
+
+  async swipeLeft(locator, speed = 'slow') {
+    await element(this._detectLocator(locator)).swipe('left', speed);
+  }
+
+  async swipeRight(locator, speed = 'slow') {
+    await element(this._detectLocator(locator)).swipe('right', speed);
+  }
+
+  
 
   /**
    * @param locator
    * @param sec
    */
   async waitForElement(locator, sec = 5) {
-    return waitFor(element(detectLocator(locator))).toExist().withTimeout(sec * 1000);
+    return waitFor(element(this._detectLocator(locator))).toExist().withTimeout(sec * 1000);
   }
 
   async waitForElementVisible(locator, sec = 5) {
-    return waitFor(element(detectLocator(locator))).toBeVisible().withTimeout(sec * 1000);
+    return waitFor(element(this._detectLocator(locator))).toBeVisible().withTimeout(sec * 1000);
   }
 
   async waitToHide(locator, sec = 5) {
-    return waitFor(element(detectLocator(locator))).toBeNotVisible().withTimeout(sec * 1000);
+    return waitFor(element(this._detectLocator(locator))).toBeNotVisible().withTimeout(sec * 1000);
   }
-}
-
-function detectLocator(locator) {
-  if (typeof locator === 'object') {
-    if (locator.id) return by.id(locator.id);
-    if (locator.label) return by.label(locator.label);
-    if (locator.text) return by.text(locator.text);
-    if (locator.type) return by.type(locator.type);
-    if (locator.traits) return by.traits(locator.traits);
-    return locator;
+  
+  _detectLocator(locator) {
+    if (typeof locator === 'object') {
+      if (locator.android && this.device.getPlatform() === 'android') return this._detectLocator(locator.android);
+      if (locator.ios && this.device.getPlatform() === 'ios') return this._detectLocator(locator.ios);
+      if (locator.id) return by.id(locator.id);
+      if (locator.label) return by.label(locator.label);
+      if (locator.text) return by.text(locator.text);
+      if (locator.type) return by.type(locator.type);
+      if (locator.traits) return by.traits(locator.traits);
+      return locator;
+    }
+  
+    if (locator[0] === '#') {
+      return by.id(locator.slice(1));
+    }
+    if (locator[0] === '~') {
+      return by.label(locator.slice(1));
+    }
+    return by.text(locator);
   }
-
-  if (locator[0] === '#') {
-    return by.id(locator.slice(1));
-  }
-  if (locator[0] === '~') {
-    return by.label(locator.slice(1));
-  }
-  return by.text(locator);
 }
 
 module.exports = Detox;
--- test/Detox_helper_test.js
+++ test/Detox_helper_test.js
@@ -1,5 +1,5 @@
 const Detox = require('../Detox');
-let configuration = process.env.CONF || 'android.emu.debug';
+let configuration = process.env.CONF || 'android.emu.release';
 
 let I;
 
@@ -8,7 +8,7 @@ describe('Detox', function () {
   this.timeout(0);
 
   
-  before(() => {
+  before(async () => {
     global.codecept_dir = __dirname;
     I = new Detox({
       configuration,
@@ -21,7 +21,9 @@ describe('Detox', function () {
     await I._before();
   });
 
-  afterEach(() => I._after());
+  afterEach(async () => await I._after());
+
+  after(async () => await I._afterSuite());
   
   describe('Sanity', () => {
     beforeEach(async () => {
@@ -45,6 +47,126 @@ describe('Detox', function () {
     });
   });
     
+  describe('Matchers', () => {
+    beforeEach(async () => {
+      await I.click('Matchers');
+    });
+  
+    it('should match elements by (accessibility) label', async () => {
+      await I.click('~Label');
+      await I.see('Label Working!!!');
+    });
+  
+    it('should match elements by (accessibility) id', async () => {
+      await I.click('#UniqueId345');
+      await I.see('ID Working!!!');
+    });
+  
+    xit('[WIP] should match elements by index', async () => {
+      const index = device.getPlatform() === 'ios' ? 2 : 0;
+      await element(by.text('Index')).atIndex(index).tap();
+      await expect(element(by.text('First button pressed!!!'))).toBeVisible();
+    });
+  
+    it('should match elements by type (native class)', async () => {
+      const type = { 
+        android: { type: 'android.widget.ImageView' },
+        ios: { type: 'RCTImageView' },
+      }
+      I.seeElement(type);
+      I.click(type);
+      I.dontSeeElement(type);
+    });
+  
+  });
+  
+  describe('Actions', () => {
+    beforeEach(async () => {
+      await device.reloadReactNative();
+      await I.click('Actions');
+    });
+  
+    it('should tap on an element', async () => {
+      await I.click('Tap Me');
+      await I.see('Tap Working!!!');
+    });
+  
+    it('should long press on an element', async () => {
+      await I.longPress('Tap Me');
+      await I.see('Long Press Working!!!');
+    });
+  
+    it('should long press with duration on an element', async () => {
+      await I.longPress('Long Press Me 1.5s', 1.5);
+      await I.see('Long Press With Duration Working!!!');
+    });
+  
+    it('should multi tap on an element', async () => {
+      await I.multiTap('#UniqueId819', 3);
+      await I.see('Taps: 3', '#UniqueId819');
+    });
+  
+    it('should tap on an element at point', async () => {
+      await I.clickAtPoint('#View7990', 180, 160);
+      await I.see('Taps: 1', '#UniqueId819');
+    });
+  
+    it('should type in an element', async () => {
+      await I.appendField('#UniqueId937', 'passcode');
+      await I.see('Type Working!!!');
+    });
+  
+    xit('should press the backspace key on an element', async () => {
+      await element(by.id('UniqueId937')).tap();
+      await element(by.id('UniqueId937')).typeText('testx');
+      await element(by.id('UniqueId937')).tapBackspaceKey();
+      await expect(element(by.text('test'))).toBeVisible();
+    });
+  
+    xit('should press the return key on an element', async () => {
+      await element(by.id('UniqueId937')).tap();
+      await element(by.id('UniqueId937')).tapReturnKey();
+      await expect(element(by.text('Return Working!!!'))).toBeVisible();
+    });
+  
+    it('should clear text in an element', async () => {
+      await I.clearField('#UniqueId005');
+      await I.see('Clear Working!!!');
+    });
+  
+    it('should replace text in an element', async () => {
+      await I.fillField('#UniqueId006', 'replaced_test');
+      await I.see('Replace Working!!!');
+    });
+    
+    // edges: 'top'/'bottom'/'left'/'right'
+    it('should scroll to edge', async () => {
+      await I.dontSee('Text8');
+      await I.scrollBottom('#ScrollView161');
+      await I.see('Text8');
+      await I.scrollTop('#ScrollView161');
+      await I.see('Text1');
+    });
+  
+    // TODO - swipe is not good enough for triggering pull to refresh. need to come up with something better
+    // directions: 'up'/'down'/'left'/'right', speed: 'fast'/'slow'
+    it('should swipe down until pull to reload is triggered', async () => {
+      await I.swipeDown('#ScrollView799');
+      await I.see('PullToReload Working!!!');
+    });
+  
+    xit('should not wait for long timeout (>1.5s)', async () => {
+      await element(by.id('WhyDoAllTheTestIDsHaveTheseStrangeNames')).tap();
+      await expect(element(by.id('WhyDoAllTheTestIDsHaveTheseStrangeNames'))).toBeVisible();
+    });
+  
+    xit(':ios: should zoom in and out the pinchable scrollview', async () => {
+      await element(by.id('PinchableScrollView')).pinchWithAngle('outward', 'slow', 0);
+      await expect(element(by.id('UniqueId007'))).toBeNotVisible();
+      await element(by.id('PinchableScrollView')).pinchWithAngle('inward', 'slow', 0);
+      await expect(element(by.id('UniqueId007'))).toBeVisible();
+    });
+  });
 
   describe('#wait', () => {
     beforeEach(async () => {
