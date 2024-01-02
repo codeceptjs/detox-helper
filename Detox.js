11d8cd76f9cd730fd2309b7ae1a201bb06e5509a
kobenguyent <kobenguyent@gmail.com>
2024-01-02 10:46:53 +0100

    fix: require the proper detox version

--- Detox.js
+++ Detox.js
@@ -10,26 +10,26 @@ let waitFor;
 /**
  * This is a wrapper on top of [Detox](https://github.com/wix/Detox) library, aimied to unify testing experience for CodeceptJS framework.
  * Detox provides a grey box testing for mobile applications, playing especially good for React Native apps.
- * 
+ *
  * Detox plays quite differently from Appium. To establish detox testing you need to build a mobile application in a special way to inject Detox code.
  * This why **Detox is grey box testing** solution, so you need access to application source code, and a way to build and execute it on emulator.
- * 
+ *
  * Comparing to Appium, Detox runs faster and more stable but requires an additional setup for build.
- * 
+ *
  * ### Setup
- * 
+ *
  * 1. [Install and configure Detox for iOS](https://github.com/wix/Detox/blob/master/docs/Introduction.GettingStarted.md) and [Android](https://github.com/wix/Detox/blob/master/docs/Introduction.Android.md)
  * 2. [Build an application](https://github.com/wix/Detox/blob/master/docs/Introduction.GettingStarted.md#step-4-build-your-app-and-run-detox-tests) using `detox build` command.
- * 3. Install [CodeceptJS](https://codecept.io) and detox-helper: 
- * 
+ * 3. Install [CodeceptJS](https://codecept.io) and detox-helper:
+ *
  * ```
  * npm i @codeceptjs/detox-helper --save
  * ```
- * 
+ *
  * Detox configuration is required in `package.json` under `detox` section.
- * 
+ *
  * If you completed step 1 and step 2 you should have a configuration similar this:
- * 
+ *
  * ```js
  *  "detox": {
  *    "configurations": {
@@ -42,33 +42,33 @@ let waitFor;
  *    }
  *  }
  * ```
- * 
- * 
+ *
+ *
  * ### Configuration
- * 
+ *
  * Besides Detox configuration, CodeceptJS should also be configured to use Detox.
- * 
+ *
  * In `codecept.conf.js` enable Detox helper:
- * 
+ *
  * ```js
  * helpers: {
  *    Detox: {
  *      require: '@codeceptjs/detox-helper',
- *      configuration: '<detox-configuration-name>', 
- *    }   
+ *      configuration: '<detox-configuration-name>',
+ *    }
  * }
- * 
+ *
  * ```
- * 
+ *
  * It's important to specify a package name under `require` section and current detox configuration taken from `package.json`.
- * 
+ *
  * Options:
- * 
+ *
  * * `configuration` - a detox configuration name. Required.
  * * `reloadReactNative` - should be enabled for React Native applications.
  * * `reuse` - reuse application for tests. By default, Detox reinstalls and relaunches app.
  * * `registerGlobals` - (default: true) Register Detox helper functions `by`, `element`, `expect`, `waitFor` globally.
- * 
+ *
  */
 class Detox extends Helper {
 
@@ -124,7 +124,7 @@ class Detox extends Helper {
     try {
       require('detox');
     } catch (e) {
-      return ['detox@^12'];
+      return ['detox@^19'];
     }
   }
 
@@ -151,12 +151,12 @@ class Detox extends Helper {
 
   /**
   * Saves a screenshot to the output dir
-  * 
+  *
   * ```js
   * I.saveScreenshot('main-window.png');
   * ```
-  * 
-  * @param {string} name 
+  *
+  * @param {string} name
   */
   async saveScreenshot(name) {
     return this.device.takeScreenshot(name);
@@ -196,7 +196,7 @@ class Detox extends Helper {
 
   /**
   * Relaunches an application.
-  * 
+  *
   * ```js
   * I.relaunchApp();
   * ```
@@ -207,7 +207,7 @@ class Detox extends Helper {
 
   /**
   * Launches an application. If application instance already exists, use [relaunchApp](#relaunchApp).
-  * 
+  *
   * ```js
   * I.launchApp();
   * ```
@@ -219,7 +219,7 @@ class Detox extends Helper {
   /**
   * Installs a configured application.
   * Application is installed by default.
-  * 
+  *
   * ```js
   * I.installApp();
   * ```
@@ -230,7 +230,7 @@ class Detox extends Helper {
 
   /**
    * Shakes the device.
-   * 
+   *
    * ```js
    * I.shakeDevice();
    * ```
@@ -241,7 +241,7 @@ class Detox extends Helper {
 
   /**
   * Goes back on Android
-  * 
+  *
   * ```js
   * I.goBack(); // on Android only
   * ```
@@ -252,7 +252,7 @@ class Detox extends Helper {
 
   /**
   * Switches device to landscape orientation
-  * 
+  *
   * ```js
   * I.setLandscapeOrientation();
   * ```
@@ -263,7 +263,7 @@ class Detox extends Helper {
 
   /**
    * Switches device to portrait orientation
-   * 
+   *
    * ```js
    * I.setPortraitOrientation();
    * ```
@@ -313,13 +313,13 @@ class Detox extends Helper {
 
 
   /**
-   * Taps on an element. 
+   * Taps on an element.
    * Element can be located by its text or id or accessibility id.
-   * 
+   *
    * The second parameter is a context element to narrow the search.
-   * 
+   *
    * Same as [click](#click)
-   * 
+   *
    * ```js
    * I.tap('Login'); // locate by text
    * I.tap('~nav-1'); // locate by accessibility label
@@ -327,9 +327,9 @@ class Detox extends Helper {
    * I.tap('Login', '#nav'); // locate by text inside #nav
    * I.tap({ ios: 'Save', android: 'SAVE' }, '#main'); // different texts on iOS and Android
    * ```
-   * 
-   * @param {CodeceptJS.LocatorOrString} locator 
-   * @param {CodeceptJS.LocatorOrString | null} [context=null] 
+   *
+   * @param {CodeceptJS.LocatorOrString} locator
+   * @param {CodeceptJS.LocatorOrString | null} [context=null]
    */
   tap(locator, context = null) {
     return this.click(locator, context);
@@ -338,19 +338,19 @@ class Detox extends Helper {
   /**
    * Multi taps on an element.
    * Element can be located by its text or id or accessibility id.
-   * 
+   *
    * Set the number of taps in second argument.
    * Optionally define the context element by third argument.
-   * 
+   *
    * ```js
    * I.multiTap('Login', 2); // locate by text
    * I.multiTap('~nav', 2); // locate by accessibility label
    * I.multiTap('#user', 2); // locate by id
    * I.multiTap('Update', 2, '#menu'); // locate by id
    * ```
-   * 
+   *
    * @param {CodeceptJS.LocatorOrString} locator element to locate
-   * @param {number} num number of taps 
+   * @param {number} num number of taps
    * @param {CodeceptJS.LocatorOrString | null} [context=null] context element
    */
   async multiTap(locator, num, context = null) {
@@ -361,16 +361,16 @@ class Detox extends Helper {
 
   /**
    * Taps an element and holds for a requested time.
-   * 
+   *
    * ```js
    * I.longPress('Login', 2); // locate by text, hold for 2 seconds
    * I.longPress('~nav', 1); // locate by accessibility label, hold for second
    * I.longPress('Update', 2, '#menu'); // locate by text inside #menu, hold for 2 seconds
    * ```
-   * 
+   *
    * @param {CodeceptJS.LocatorOrString} locator element to locate
    * @param {number} sec number of seconds to hold tap
-   * @param {CodeceptJS.LocatorOrString | null} [context=null] context element 
+   * @param {CodeceptJS.LocatorOrString | null} [context=null] context element
    */
   async longPress(locator, sec, context = null) {
     locator = this._detectLocator(locator, 'text');
@@ -380,13 +380,13 @@ class Detox extends Helper {
 
 
   /**
-   * Clicks on an element. 
+   * Clicks on an element.
    * Element can be located by its text or id or accessibility id
-   * 
+   *
    * The second parameter is a context (id | type | accessibility id) to narrow the search.
-   * 
+   *
    * Same as [tap](#tap)
-   * 
+   *
    * ```js
    * I.click('Login'); // locate by text
    * I.click('~nav-1'); // locate by accessibility label
@@ -394,9 +394,9 @@ class Detox extends Helper {
    * I.click('Login', '#nav'); // locate by text inside #nav
    * I.click({ ios: 'Save', android: 'SAVE' }, '#main'); // different texts on iOS and Android
    * ```
-   * 
-   * @param {CodeceptJS.LocatorOrString} locator 
-   * @param {CodeceptJS.LocatorOrString | null} [context=null] 
+   *
+   * @param {CodeceptJS.LocatorOrString} locator
+   * @param {CodeceptJS.LocatorOrString | null} [context=null]
    */
   async click(locator, context = null) {
     locator = this._detectLocator(locator, 'text');
@@ -405,19 +405,19 @@ class Detox extends Helper {
   }
 
   /**
-   * Clicks on an element. 
+   * Clicks on an element.
    * Element can be located by its label
-   * 
+   *
    * The second parameter is a context (id | type | accessibility id) to narrow the search.
-   * 
-   * 
+   *
+   *
    * ```js
    * I.tapByLabel('Login'); // locate by text
    * I.tapByLabel('Login', '#nav'); // locate by text inside #nav
    * ```
-   * 
-   * @param {CodeceptJS.LocatorOrString} locator 
-   * @param {CodeceptJS.LocatorOrString | null} [context=null] 
+   *
+   * @param {CodeceptJS.LocatorOrString} locator
+   * @param {CodeceptJS.LocatorOrString | null} [context=null]
    */
   async tapByLabel(locator, context = null) {
     locator = this._detectLocator(locator, 'label');
@@ -428,12 +428,12 @@ class Detox extends Helper {
   /**
   * Performs click on element with horizontal and vertical offset.
   * An element is located by text, id, accessibility id.
-  * 
+  *
   * ```js
   * I.clickAtPoint('Save', 10, 10);
   * I.clickAtPoint('~save', 10, 10); // locate by accessibility id
   * ```
-  * 
+  *
   * @param {CodeceptJS.LocatorOrString} locator
   * @param {number} [x=0] horizontal offset
   * @param {number} [y=0] vertical offset
@@ -445,15 +445,15 @@ class Detox extends Helper {
   /**
    * Checks text to be visible.
    * Use second parameter to narrow down the search.
-   * 
+   *
    * ```js
    * I.see('Record created');
    * I.see('Record updated', '#message');
    * I.see('Record deleted', '~message');
    * ```
-   * 
+   *
    * @param {string} text to check visibility
-   * @param {CodeceptJS.LocatorOrString | null} [context=null] element inside which to search for text 
+   * @param {CodeceptJS.LocatorOrString | null} [context=null] element inside which to search for text
    */
   see(text, context = null) {
     if (context) {
@@ -465,7 +465,7 @@ class Detox extends Helper {
   /**
    * Checks text not to be visible.
    * Use second parameter to narrow down the search.
-   * 
+   *
    * ```js
    * I.dontSee('Record created');
    * I.dontSee('Record updated', '#message');
@@ -483,13 +483,13 @@ class Detox extends Helper {
   /**
    * Checks for visibility of an element.
    * Use second parameter to narrow down the search.
-   * 
+   *
    * ```js
    * I.seeElement('~edit'); // located by accessibility id
    * I.seeElement('~edit', '#menu'); // element inside #menu
    * ```
-   * 
-   * @param {CodeceptJS.LocatorOrString} locator element to locate 
+   *
+   * @param {CodeceptJS.LocatorOrString} locator element to locate
    * @param {CodeceptJS.LocatorOrString | null} [context=null] context element
    */
   seeElement(locator, context = null) {
@@ -500,13 +500,13 @@ class Detox extends Helper {
 
   /**
    * Checks if an element exists.
-   * 
+   *
    * ```js
    * I.checkIfElementExists('~edit'); // located by accessibility id
    * I.checkIfElementExists('~edit', '#menu'); // element inside #menu
    * ```
-   * 
-   * @param {CodeceptJS.LocatorOrString} locator element to locate 
+   *
+   * @param {CodeceptJS.LocatorOrString} locator element to locate
    * @param {CodeceptJS.LocatorOrString | null} [context=null] context element
    */
   checkIfElementExists(locator, context = null) {
@@ -519,7 +519,7 @@ class Detox extends Helper {
   /**
    * Checks that element is not visible.
    * Use second parameter to narrow down the search.
-   * 
+   *
    * ```js
    * I.dontSeeElement('~edit'); // located by accessibility id
    * I.dontSeeElement('~edit', '#menu'); // element inside #menu
@@ -536,13 +536,13 @@ class Detox extends Helper {
   /**
    * Checks for existence of an element. An element can be visible or not.
    * Use second parameter to narrow down the search.
-   * 
+   *
    * ```js
    * I.seeElementExists('~edit'); // located by accessibility id
    * I.seeElementExists('~edit', '#menu'); // element inside #menu
    * ```
-   * 
-   * @param {CodeceptJS.LocatorOrString} locator element to locate 
+   *
+   * @param {CodeceptJS.LocatorOrString} locator element to locate
    * @param {CodeceptJS.LocatorOrString} [context=null]  context element
    */
   seeElementExists(locator, context = null) {
@@ -554,13 +554,13 @@ class Detox extends Helper {
   /**
    * Checks that element not exists.
    * Use second parameter to narrow down the search.
-   * 
+   *
    * ```js
    * I.dontSeeElementExist('~edit'); // located by accessibility id
    * I.dontSeeElementExist('~edit', '#menu'); // element inside #menu
    * ```
-   * 
-   * @param {CodeceptJS.LocatorOrString} locator element to locate 
+   *
+   * @param {CodeceptJS.LocatorOrString} locator element to locate
    * @param {CodeceptJS.LocatorOrString} [context=null] context element
    */
   dontSeeElementExists(locator, context = null) {
@@ -572,15 +572,15 @@ class Detox extends Helper {
   /**
    * Fills in text field in an app.
    * A field can be located by text, accessibility id, id.
-   * 
+   *
    * ```js
    * I.fillField('Username', 'davert');
    * I.fillField('~name', 'davert');
    * I.fillField({ android: 'NAME', ios: 'name' }, 'davert');
    * ```
-   * 
+   *
    * @param {CodeceptJS.LocatorOrString} field an input element to fill in
-   * @param {string} value value to fill 
+   * @param {string} value value to fill
    */
   async fillField(field, value) {
     const locator = this._detectLocator(field, 'text');
@@ -591,13 +591,13 @@ class Detox extends Helper {
   /**
    * Taps return key.
    * A field can be located by text, accessibility id, id.
-   * 
+   *
    * ```js
    * I.tapReturnKey('Username');
    * I.tapReturnKey('~name');
    * I.tapReturnKey({ android: 'NAME', ios: 'name' });
    * ```
-   * 
+   *
    * @param {CodeceptJS.LocatorOrString} field an input element to fill in
    */
   async tapReturnKey(field) {
@@ -608,11 +608,11 @@ class Detox extends Helper {
   /**
    * Clears a text field.
    * A field can be located by text, accessibility id, id.
-   * 
+   *
    * ```js
    * I.clearField('~name');
-   * ``` 
-   * 
+   * ```
+   *
    * @param {CodeceptJS.LocatorOrString} field an input element to clear
    */
   async clearField(field) {
@@ -624,13 +624,13 @@ class Detox extends Helper {
   /**
    * Appends text into the field.
    * A field can be located by text, accessibility id, id.
-   * 
+   *
    * ```js
    * I.appendField('name', 'davert');
    * ```
-   * 
-   * @param {CodeceptJS.LocatorOrString} field 
-   * @param {string} value 
+   *
+   * @param {CodeceptJS.LocatorOrString} field
+   * @param {string} value
    */
   async appendField(field, value) {
     const locator = this._detectLocator(field, 'text');
@@ -640,12 +640,12 @@ class Detox extends Helper {
 
   /**
    * Scrolls to the top of an element.
-   * 
+   *
    * ```js
    * I.scrollUp('#container');
    * ```
-   * 
-   * @param {CodeceptJS.LocatorOrString} locator 
+   *
+   * @param {CodeceptJS.LocatorOrString} locator
    */
   async scrollUp(locator) {
     await element(this._detectLocator(locator)).scrollTo('top');
@@ -653,12 +653,12 @@ class Detox extends Helper {
 
   /**
    * Scrolls to the bottom of an element.
-   * 
+   *
    * ```js
    * I.scrollDown('#container');
    * ```
-   * 
-   * @param {CodeceptJS.LocatorOrString} locator 
+   *
+   * @param {CodeceptJS.LocatorOrString} locator
    */
   async scrollDown(locator) {
     await element(this._detectLocator(locator)).scrollTo('bottom');
@@ -666,12 +666,12 @@ class Detox extends Helper {
 
   /**
    * Scrolls to the left of an element.
-   * 
+   *
    * ```js
    * I.scrollLeft('#container');
    * ```
-   * 
-   * @param {CodeceptJS.LocatorOrString} locator 
+   *
+   * @param {CodeceptJS.LocatorOrString} locator
    */
   async scrollLeft(locator) {
     await element(this._detectLocator(locator)).scrollTo('left');
@@ -680,12 +680,12 @@ class Detox extends Helper {
 
   /**
    * Scrolls to the right of an element.
-   * 
+   *
    * ```js
    * I.scrollRight('#container');
    * ```
-   * 
-   * @param {CodeceptJS.LocatorOrString} locator 
+   *
+   * @param {CodeceptJS.LocatorOrString} locator
    */
   async scrollRight(locator) {
     await element(this._detectLocator(locator)).scrollTo('right');
@@ -695,11 +695,11 @@ class Detox extends Helper {
   /**
    * Performs a swipe up inside an element.
    * Can be `slow` or `fast` swipe.
-   * 
+   *
    * ```js
    * I.swipeUp('#container');
    * ```
-   * 
+   *
    * @param {CodeceptJS.LocatorOrString} locator an element on which to perform swipe
    * @param {string} [speed='slow'] a speed to perform: `slow` or `fast`.
    */
@@ -711,11 +711,11 @@ class Detox extends Helper {
   /**
    * Performs a swipe up inside an element.
    * Can be `slow` or `fast` swipe.
-   * 
+   *
    * ```js
    * I.swipeUp('#container');
    * ```
-   * 
+   *
    * @param {CodeceptJS.LocatorOrString} locator an element on which to perform swipe
    * @param {string} [speed='slow'] a speed to perform: `slow` or `fast`.
    */
@@ -727,11 +727,11 @@ class Detox extends Helper {
   /**
    * Performs a swipe up inside an element.
    * Can be `slow` or `fast` swipe.
-   * 
+   *
    * ```js
    * I.swipeUp('#container');
    * ```
-   * 
+   *
    * @param {CodeceptJS.LocatorOrString} locator an element on which to perform swipe
    * @param {string} [speed='slow'] a speed to perform: `slow` or `fast`.
    */
@@ -743,11 +743,11 @@ class Detox extends Helper {
   /**
    * Performs a swipe up inside an element.
    * Can be `slow` or `fast` swipe.
-   * 
+   *
    * ```js
    * I.swipeUp('#container');
    * ```
-   * 
+   *
    * @param {CodeceptJS.LocatorOrString} locator an element on which to perform swipe
    * @param {string} [speed='slow'] a speed to perform: `slow` or `fast`.
    */
@@ -757,11 +757,11 @@ class Detox extends Helper {
 
   /**
    * Waits for number of seconds
-   * 
+   *
    * ```js
    * I.wait(2); // waits for 2 seconds
    * ```
-   * 
+   *
    * @param {number} sec number of seconds to wait
    */
   async wait(sec) {
@@ -772,11 +772,11 @@ class Detox extends Helper {
 
   /**
    * Waits for an element to exist on page.
-   * 
+   *
    * ```js
    * I.waitForElement('#message', 1); // wait for 1 second
    * ```
-   * 
+   *
    * @param {CodeceptJS.LocatorOrString} locator an element to wait for
    * @param {number} [sec=5] number of seconds to wait, 5 by default
    */
@@ -786,11 +786,11 @@ class Detox extends Helper {
 
   /**
    * Waits for an element to be visible on page.
-   * 
+   *
    * ```js
    * I.waitForElementVisible('#message', 1); // wait for 1 second
    * ```
-   * 
+   *
    * @param {CodeceptJS.LocatorOrString} locator an element to wait for
    * @param {number} [sec=5] number of seconds to wait
    */
@@ -800,11 +800,11 @@ class Detox extends Helper {
 
   /**
    * Waits an elment to become not visible.
-   * 
+   *
    * ```js
    * I.waitToHide('#message', 2); // wait for 2 seconds
    * ```
-   * 
+   *
    * @param {CodeceptJS.LocatorOrString} locator  an element to wait for
    * @param {number} [sec=5] number of seconds to wait
    */
