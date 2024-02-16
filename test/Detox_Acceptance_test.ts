df95aabd67adccc6361014ba3452c761ba9f79eb
kobenguyent <kobenguyent@gmail.com>
2024-02-16 10:49:32 +0100

    fix: failed test - android workflow

--- test/Detox_Acceptance_test.ts
+++ test/Detox_Acceptance_test.ts
@@ -12,7 +12,7 @@ Scenario('App started', () => {
 
 Scenario('Get platform', async () => {
   const platform = await I.grabPlatform();
-  process.env.CONF.includes('ios') ? I.expectEqual(platform, 'ios') : I.expectEqual(platform, 'android');
+  process.env.CONF && process.env.CONF.includes('ios') ? I.expectEqual(platform, 'ios') : I.expectEqual(platform, 'android');
 })
 
 Scenario('Show hello screen after tap', () => {
