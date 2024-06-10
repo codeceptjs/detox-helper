14a7c050f9729c4079c4078f17138df6c4076d7e
KobeN <7845001+kobenguyent@users.noreply.github.com>
2024-06-10 13:22:13 +0200

    Update steps.d.ts

--- test/steps.d.ts
+++ test/steps.d.ts
@@ -4,7 +4,7 @@ type Detox = import('./helpers/detoxHelper');
 
 declare namespace CodeceptJS {
   interface SupportObject { I: I, current: any }
-  interface Methods extends Detox, Expect {}
+  interface Methods extends Detox, ExpectHelper {}
   interface I extends ReturnType<steps_file>, WithTranslation<Methods> {}
   namespace Translation {
     interface Actions {}
